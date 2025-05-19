eca70fce0326d426816bd073ae2133887c93cdff
carlos-urena <curena@ugr.es>
2025-05-19 13:41:53 +0200

    añado no_cache server a app ray-tracer web

--- fuentes/ray-tracer-glsl/scripts/comp-lanzar.sh
+++ fuentes/ray-tracer-glsl/scripts/comp-lanzar.sh
@@ -18,9 +18,10 @@ echo "----"
 ## si no ha habido errores, lanzar servidor
 if [ "$res" == "0" ] ; then 
    echo "Lanzando servidor, página raiz en 'localhost:8000' (o 'http://localhost:8000')" 
-   pushd ../public_html >/dev/null
-   python3 -m http.server
-   popd >/dev/null
+   # pushd ../public_html >/dev/null
+   # python3 -m http.server
+   # popd >/dev/null
+   python3 no_cache_server.py
 else
    echo "Errores: no se lanza servidor."
 fi
